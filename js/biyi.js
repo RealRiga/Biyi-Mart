556dd54f538eaace7ef8fe1cfe314f642627a99d
RealRiga <116930426+RealRiga@users.noreply.github.com>
2024-04-03 17:47:54 +0100

    changed the landingpage.html to index.html

--- js/biyi.js
+++ js/biyi.js
@@ -8,7 +8,7 @@ function myAccount() {
         if (!modal2.classList.contains("no-visible")) {
             modal2.classList.add("no-visible")
             modal2.classList.remove("now-visible")
-        } 
+        }
     } else {
         modal.classList.add("not-visible")
         modal.classList.remove("visible")
@@ -26,51 +26,10 @@ function askHelp() {
         if (!modal2.classList.contains("not-visible")) {
             modal2.classList.add("not-visible")
             modal2.classList.remove("visible")
-        } 
+        }
     } else {
         modal.classList.add("no-visible")
         modal.classList.remove("now-visible")
     }
 };
-
-// Left Arrow section
-
-
-
-
-// Right arrow section
-
-
-
-async function logJSONData(){
-    const response = await
-    fetch("http://example.com/pictures.json");
-    const jsonData = await response.json();
-    console.log(jsonData);
-}
-
-// Example POST method implementation:
-async function postData(url = "", data = {}){
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: "POST", //*GET, POST, PUT, DELETE. etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", //*default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded'.
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referre-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    
-    });
-    return response.json(); // parses JSON response into native Javascript objects
-}
-
-postData("https://dog.ceo/api", {
-    answer: 42}).then((data)=>{
-        console.log(data); //JSON data parsed by 'data.json()' call
-    });
 
